e190501f33fc47283122dafab5db531ff3ba1f14
nishino <takaya314boxing@gmail.com>
2021-06-27 09:58:18 +0000

    fix/staff_app簡単なアプリ完成

---
+++ app/controllers/staffs_controller.rb
@@ -0,0 +1,76 @@
+class StaffsController < ApplicationController
+  #before_actionで各メソッドを呼び出す前にメソッドを呼び出す
+ #onlyでどのメソッドで呼び出すか制限をかける
+before_action :set_staff, only: [:show, :update, :destroy, :edit]
+
+
+#index->データの一覧の表示
+ def index
+   #Staffのデータの全件取得
+   @staffs = Staff.all
+ end
+
+
+ def new
+   @staff = Staff.new
+ end
+
+ def show
+  #before_actionでデータの取得は完了している
+ end
+
+ #create->新規データの登録
+ def create
+    #formのデータを受け取る
+   @staff = Staff.new(staff_params)
+   if @staff.save
+     #saveが完了したら、一覧ページへリダイレクト
+     flash[:success] = '新規作成に成功しました。'
+     redirect_to staffs_url
+   else
+   #saveを失敗すると新規作成ページへ
+     render :new
+   end
+ end
+
+ #edit->編集ページの表示
+ def edit
+ #before_actionでデータの取得は完了している
+ end
+
+ def update
+    #編集データの取得
+   if @staff.update!(staff_params)
+     #updateが完了したら一覧ページへリダイレクト
+     flash[:success] = "従業員情報を更新しました。"
+     redirect_to staffs_url
+   else
+     #updateを失敗すると編集ページへ
+     render :edit     
+   end
+ end
+
+ #destroy->データの削除
+ def destroy
+   #データの削除
+   @staff.destroy
+   flash[:success] = "#{@staff.name}のデータを削除しました。"
+   #一覧ページへリダイレクト
+   redirect_to staffs_url
+ end
+
+
+
+  private
+   #strong parameters リクエストパラメターの検証（これがないとうまくいかないので注意）
+
+   def staff_params
+     params.require(:staff).permit(:name, :email, :cellphone)
+   end
+
+   #共通処理なので、before_actionで呼び出している
+   def set_staff
+   #特定データの取得
+     @staff = Staff.find(params[:id])
+   end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Staff.create!(name: "A",
+             email: "sample@email.com",
+             cellphone: "09000000")
+
+
+Staff.create!(name: "B",
+             email: "sample-1@email.com",
+             cellphone: "09011111")
+
+Staff.create!(name: "C",
+             email: "sample-2@email.com",
+             cellphone: "0902222222")           
+
+Staff.create!(name: "D",
+             email: "sample-3@email.com",
+            cellphone: "0903333333")
+
+
+Staff.create!(name: "E",
+            email: "sample@email.com",
+            cellphone: "09044444")
